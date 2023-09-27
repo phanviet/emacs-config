3e892a44c8c87ad5dd620fe8f47e57d53eb1e9d5
Viet Phan <vietphxfce@gmail.com>
2023-09-27 23:57:37 +0700

    ok

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-(set-frame-font "Monaco 12" nil t)
+(set-frame-font "Monaco 14" nil t)
 (tool-bar-mode -1)
+(setq ring-bell-function 'ignore)
 
 (defconst user-init-dir
   (cond ((boundp 'user-emacs-directory)
@@ -13,9 +14,13 @@
   (interactive "f")
   (load-file (expand-file-name file user-init-dir)))
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
+(load-user-file "common.el")
 (load-user-file "org-mode-custom.el")
-
+(load-user-file "lua.el")
 
 
 ;; GLOBAL KEY BINDINGS
@@ -23,3 +28,17 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lua-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lua.el
@@ -0,0 +1,2 @@
+(setq lua-indent-level 4)
+(setq lua-default-application "luajit")
